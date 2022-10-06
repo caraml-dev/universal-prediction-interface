7f299165b8a571d98d66c98c18cee1add4aadb61
ningjie.lee <ningjie.lee@gojek.com>
2022-10-06 11:06:24 +0800

    add readme and minor refactor

---
+++ example/README.md
@@ -0,0 +1,8 @@
+# Universal Prediction Interface Example
+
+## Go Example 
+.  
+├── go/simple  
+│   ├── client         # Go Client  
+│   └── server         # Go Upi Echo Server  
+└── README.md
--- example/go/simple/server/main.go
+++ example/go/simple/server/main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	upiv1 "github.com/caraml-dev/universal-prediction-interface/gen/go/grpc/caraml/upi/v1"
 )
 
 var (
 	port = flag.Int("port", 50051, "The server port")
 )
-
-type server struct {
-	upiv1.UnimplementedUniversalPredictionServiceServer
-}
 
 func main() {
 	flag.Parse()
